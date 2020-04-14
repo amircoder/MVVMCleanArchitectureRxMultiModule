a3cdb2ae4b3ca490a25063b1eab52d02d68b02cc
mac <appcontactng@gmail.com>
2020-04-14 16:17:06 +0430

    Adding robolectric helper classes to the core-test module.

--- core-test/src/androidTest/java/com/aba/core/ExampleInstrumentedTest.kt
+++ core-test/src/androidTest/java/com/aba/core/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.aba.core_test
+package com.aba.core
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- core-test/src/test/java/com/aba/core/ExampleUnitTest.kt
+++ core-test/src/test/java/com/aba/core/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.aba.core_test
+package com.aba.core
 
 import org.junit.Test
 
